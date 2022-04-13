8a07228ef9176731d2c05ada755e9a1c8eaf3bfd
marvinh <marvinharootoonyan@gmail.com>
2022-04-12 19:30:36 -0700

    .env

--- vgg.py
+++ vgg.py
@@ -2,24 +2,35 @@
 from tensorflow.keras.utils import to_categorical
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import numpy as np
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 
 ## Loading images and labels
 
+TRAIN_DIR = os.environ.get('TRAIN_DIR')
+TEST_DIR = os.environ.get('TEST_DIR')
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
-  '/Users/marvinharootoonyan/ct_541/classification_technique/TrainImages/',
-  seed=123,
+  TRAIN_DIR,
+  labels='inferred',
   image_size=(224, 224),
   batch_size=64)
 test_ds = tf.keras.utils.image_dataset_from_directory(
-  '/Users/marvinharootoonyan/ct_541/classification_technique/TestImages/',
-  seed=123,
+  TEST_DIR,
+  labels='inferred',
   image_size=(224, 224),
   batch_size=64)
 
 
+train_labels = np.concatenate([y for x, y in train_ds], axis=0)
+print(train_labels)
+print(len(train_labels))
 
-print(len(train_ds.class_names))
-print(len(test_ds.class_names))
-train_labels = to_categorical(train_ds.class_names, num_classes=len(train_ds.class_names))
-test_labels = to_categorical(test_ds.class_names, num_classes=len(test_ds.class_names))
+# print(len(test_ds.class_names))
+# print(train_ds.class_names)
+# train_labels = to_categorical(train_ds.class_names, num_classes=len(train_ds.class_names))
+# test_labels = to_categorical(test_ds.class_names, num_classes=len(test_ds.class_names))
