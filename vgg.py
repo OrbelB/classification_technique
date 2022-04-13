c7146ba163f69ef79a242cc504340e08e14c29c8
Orbel B <o.baghdasian@gmail.com>
2022-04-12 19:57:09 -0700

    Started Testing how to Train VGG16

--- vgg.py
+++ vgg.py
@@ -1,5 +1,7 @@
 # import tensorflow_datasets as tfds
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg16 import preprocess_input
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
@@ -30,3 +32,15 @@ train_labels = np.concatenate([y for x, y in train_ds], axis=0)
 test_labels = np.concatenate([y for x, y in test_ds], axis=0)
 train_labels = to_categorical(train_labels, num_classes=len(train_ds.class_names))
 test_labels = to_categorical(test_labels, num_classes=len(test_ds.class_names))
+
+train_ds = np.concatenate([x for x, y in train_ds], axis=0)
+test_ds = np.concatenate([x for x, y in train_ds], axis=0)
+
+base_model = VGG16(weights="imagenet", include_top=False, input_shape=train_ds[0].shape)
+base_model.trainable = False ## Not trainable weights
+
+## Preprocessing input
+train_ds = preprocess_input(train_ds) 
+test_ds = preprocess_input(test_ds)
+
+base_model.summary()
